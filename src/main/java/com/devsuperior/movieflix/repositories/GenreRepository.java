e4d14f872f5c1f025bd9a301f7c6afd7b9f1a4f7
carlos5veiga <carlos5veiga@gmail.com>
2022-02-25 15:52:30 -0300

    Fix: repositories types

--- src/main/java/com/devsuperior/movieflix/repositories/GenreRepository.java
+++ src/main/java/com/devsuperior/movieflix/repositories/GenreRepository.java
@@ -3,9 +3,9 @@ package com.devsuperior.movieflix.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.devsuperior.movieflix.entities.Role;
+import com.devsuperior.movieflix.entities.Genre;
 
 @Repository
-public interface GenreRepository extends JpaRepository<Role, Long> {
+public interface GenreRepository extends JpaRepository<Genre, Long> {
 
 }
